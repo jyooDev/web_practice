12f7257245604d3a654b5eae25e7ff2dfd9fa728
jyooDev <gina07099@gmail.com>
2025-04-30 17:43:47 -0500

    js mini project rolling dice

---
+++ javascript_foundation/mini_projects/05_rolling_dice/script.js
@@ -0,0 +1,28 @@
+
+const diceCount = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+};
+
+function rollDice(){
+    let roll = Math.ceil(Math.random()*6);
+    // console.log(`dice num: ${num}`);
+    diceCount[roll]++;
+}
+
+
+function displayScore(){
+    for (const roll in diceCount){
+        console.log(`${roll}: ${diceCount[roll]} times`);
+    }
+}
+
+for(let i = 0; i < 100000; i++)
+{
+    rollDice();
+}
+displayScore();
